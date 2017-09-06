2f64a71a1e446ae126138c780660c815a642c7c7
bahrus <anderson.bruce.b@gmail.com>
2017-09-06 06:01:55 -0400

    xtal-decorator

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
+[![Published on webcomponents.org](https://img.shields.io/badge/webcomponents.org-published-blue.svg)](https://www.webcomponents.org/element/bahrus/xtal-decorator)
+
 # \<xtal-decorator\>
 
 Attach event handlers, properties to a neighboring custom element.
 
-\<xtal-decorator\> provides some useful features for "decorating" neighboring custom elements, similar in concept to class decorators.  It is most focused on being able to latch custom element behavior onto a polymer dom-bind element, but it can generally be used for customizing the behavior of any custom element "inline," without extending it into another custom element.  Methods can be attached, where "this" refers to the actual custom element it is attached to.
+\<xtal-decorator\> provides the ability to "decorate" neighboring custom element instances.  It is most focused on being able to latch custom element behavior onto a polymer dom-bind element instance, but it can generally be used for customizing the behavior of any custom element "inline," without extending it into another custom element.  Methods can be attached, where "this" refers to the actual custom element it is attached to.
 
 Properties can also be configured this way, pulling in data from the global scope.
-
 
 
 ## Install the Polymer-CLI
--- xtal-decorator.ts
+++ xtal-decorator.ts
@@ -15,7 +15,8 @@ module xtal.elements {
         static get observedAttributes() {
             return [
                 /** @type {string} 
-                 * Selector to search for within the parent element
+                 * Selector to search for within the parent element.  Default value:  dom-bind
+                 * This will select the target elements(s) to which properties and methods will be attached.
                  */
                 'selector',
             ];
@@ -34,100 +35,108 @@ module xtal.elements {
             // }
             // if(!nextSibling) return;
             const targets = this.parentElement.querySelectorAll(this._CssSelector);
-            console.log('targets length = ' + targets.length);
             const scriptTag = this.querySelector("script");
             const innerText = scriptTag.innerText;
             const objectsToMerge = eval(innerText) as any[];
-            for (let i = 0, ii = targets.length; i < ii; i++) {
-                const target = targets[i];
-                for (let j = 0, jj = objectsToMerge.length; j < jj; j++) {
-
-                    const objectToMerge = objectsToMerge[j];
-                    console.log({objectToMerge: objectToMerge});
-                    for (var key in objectToMerge) {
-                        const val = objectToMerge[key];
-                        switch (typeof val) {
-                            case 'function':
+            let propertiesToSet;
+            for (let j = 0, jj = objectsToMerge.length; j < jj; j++) {
+                const objectToMerge = objectsToMerge[j];
+                for (var key in objectToMerge) {
+                    const val = objectToMerge[key];
+                    switch (typeof val) {
+                        case 'function':
+                            for(let i = 0, ii = targets.length; i < ii; i++){
+                                const target = targets[i];
                                 Object.defineProperty(target, key, {
                                     enumerable: false,
                                     configurable: true,
                                     writable: true,
                                     value: val,
                                 });
-                                break;
-                            case 'object':
-                                if(key === 'properties'){
-                                    debugger;
-                                    Object.assign(target, val);
+                            }
+
+                            break;
+                        case 'object':
+                            if(key === 'properties'){
+                                if(!propertiesToSet) {
+                                    propertiesToSet = val;
+                                }else{
+                                    this.mergeDeep(propertiesToSet, val);
                                 }
-                                break;
                                 
+                                //Object.assign(target, val);
+                            }
+                            break;
                             
-                        }
+                        
                     }
                 }
-
-                // objectsToMerge.forEach(obj =>{
-                //     this.mergeDeep(target, obj);
-                // })
+                                }
+            for (let i = 0, ii = targets.length; i < ii; i++) {
+                const target = targets[i];
+                if(target['setProperties']){
+                    target['setProperties'](propertiesToSet);
+                }else{
+                    Object.assign(target, propertiesToSet);
+                }
             }
 
 
         }
 
-        // /**
-        //  * Deep merge two objects.
-        //  * Inspired by Stackoverflow.com/questions/27936772/deep-object-merging-in-es6-es7
-        //  * @param target
-        //  * @param source
-        //  * 
-        //  */
-        // mergeDeep(target, source) {
-        //     if(typeof target !== 'object') return;
-        //     if(typeof source !== 'object') return;
-        //     for (const key in source) {
-        //         const sourceVal = source[key];
-        //         const targetVal = target[key];
-        //         if(!sourceVal) continue; //TODO:  null out property?
-        //         if(!targetVal){
-        //             console.log(key);
-        //             target[key] = sourceVal;
-        //             continue;
-        //         }
-        //         if(Array.isArray(sourceVal) && Array.isArray(targetVal)){
-        //             //warning!! code below not yet tested
-        //             if(targetVal.length > 0 && typeof targetVal[0].id === 'undefined') continue;
-        //             for(var i = 0, ii = sourceVal.length; i < ii; i++){
-        //                 const srcEl = sourceVal[i];
-        //                 if(typeof srcEl.id === 'undefined') continue;
-        //                 const targetEl = targetVal.find(function(el){return el.id === srcEl.id;});
-        //                 if(targetEl){
-        //                     this.mergeDeep(targetEl, srcEl);
-        //                 }else{
-        //                     targetVal.push(srcEl);
-        //                 }
-        //             }
-        //             continue;
-        //         }
-        //         switch(typeof sourceVal){
-        //             case 'object':
-        //                 switch(typeof targetVal){
-        //                     case 'object':
-        //                         this.mergeDeep(targetVal, sourceVal);
-        //                         break;
-        //                     default:
-        //                         console.log(key);
-        //                         target[key] = sourceVal;
-        //                         break;
-        //                 }
-        //                 break;
-        //             default:
-        //                 console.log(key);
-        //                 target[key] = sourceVal;
-        //         }
-        //     }
-        //     return target;
-        // }
+        /**
+         * Deep merge two objects.
+         * Inspired by Stackoverflow.com/questions/27936772/deep-object-merging-in-es6-es7
+         * @param target
+         * @param source
+         * 
+         */
+        mergeDeep(target, source) {
+            if(typeof target !== 'object') return;
+            if(typeof source !== 'object') return;
+            for (const key in source) {
+                const sourceVal = source[key];
+                const targetVal = target[key];
+                if(!sourceVal) continue; //TODO:  null out property?
+                if(!targetVal){
+                    console.log(key);
+                    target[key] = sourceVal;
+                    continue;
+                }
+                if(Array.isArray(sourceVal) && Array.isArray(targetVal)){
+                    //warning!! code below not yet tested
+                    if(targetVal.length > 0 && typeof targetVal[0].id === 'undefined') continue;
+                    for(var i = 0, ii = sourceVal.length; i < ii; i++){
+                        const srcEl = sourceVal[i];
+                        if(typeof srcEl.id === 'undefined') continue;
+                        const targetEl = targetVal.find(function(el){return el.id === srcEl.id;});
+                        if(targetEl){
+                            this.mergeDeep(targetEl, srcEl);
+                        }else{
+                            targetVal.push(srcEl);
+                        }
+                    }
+                    continue;
+                }
+                switch(typeof sourceVal){
+                    case 'object':
+                        switch(typeof targetVal){
+                            case 'object':
+                                this.mergeDeep(targetVal, sourceVal);
+                                break;
+                            default:
+                                console.log(key);
+                                target[key] = sourceVal;
+                                break;
+                        }
+                        break;
+                    default:
+                        console.log(key);
+                        target[key] = sourceVal;
+                }
+            }
+            return target;
+        }
     }
     customElements.define(XtalDecorator.is, XtalDecorator);
 }
